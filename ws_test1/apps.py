3b5715e9dd1d3c5314e0c1324353853a85d5f3d1
sorennx <sorennx@gmail.com>
2022-03-02 22:10:37 +0100

    Initial commit

---
+++ ws_test1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WsTest1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ws_test1'
---
+++ ws_test1/urls.py
@@ -0,0 +1,11 @@
+from django.urls import re_path
+from . import views
+urlpatterns = [
+    re_path(r'^$',views.index, name='index'),
+# ex: /polls/5/
+re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+# ex: /polls/5/results/
+re_path(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+# ex: /polls/5/vote/
+re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
