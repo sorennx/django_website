125692ca6b472b9d3082a4729ed9cda2a6505335
sorennx <sorennx@gmail.com>
2022-03-03 14:05:24 +0100

    v0.0.1 - initial works on home page

--- ws_test1/urls.py
+++ ws_test1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
 from . import views
 urlpatterns = [
-    re_path(r'^$',views.home, name='index'),
+    #re_path(r'^$',views.home, name='index'),
     # ex: /polls/5/
     re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
