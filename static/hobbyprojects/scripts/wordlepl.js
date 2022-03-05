a0ebb95090b42078e559cb94bb61cfdd4343b9b4
sorennx <sorennx@gmail.com>
2022-03-05 14:55:40 +0100

    v0.2.2 - disabling rows

--- static/hobbyprojects/scripts/wordlepl.js
+++ static/hobbyprojects/scripts/wordlepl.js
@@ -12,4 +12,62 @@ function test()
 
 }
 
+function blockRow(row)
+{
+    document.getElementsByClassName(row).disabled=true;
+}
 
+function wordlepl()
+{
+    rowList = [document.getElementsByClassName('tile_input_r1'),
+        document.getElementsByClassName('tile_input_r2'),document.getElementsByClassName('tile_input_r2'),
+        document.getElementsByClassName('tile_input_r3'),document.getElementsByClassName('tile_input_r4'),
+        document.getElementsByClassName('tile_input_r5'),document.getElementsByClassName('tile_input_6')]
+
+    for (let i=1; i<rowList.length; i++){
+        blockRow(rowList[i]);
+    }
+}
+
+
+
+
+    // row = document.getElementsByClassName("tile_input_r1");
+    // let i=1;
+    // let check1 = false;
+    // while (i<=10) {
+    //     {#task(i);#}
+    //     checkRow(row,i);
+    //     i+=1;
+    //     if (check1===true){
+    //         break;
+    //     }
+    //  }
+    //  test();
+
+    function task(i) {
+      setTimeout(function() {
+          console.log(i);
+      }, 2000 * i);
+    }
+
+    function checkRow(row,i){
+        setTimeout(function() {
+            for (let i = 0; i < row.length; i++) {
+                if (!row[i].value){
+                    continue;
+                    check1=false;
+
+
+
+                }
+                else{
+                    check1=true;
+                }
+
+
+        }
+             console.log(check1);
+        },2000*i);
+
+    }
